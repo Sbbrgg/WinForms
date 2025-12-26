345a0fb4347855f49612ba82b0633ea67873f2fa
un <un@DESKTOP-JAU235M>
2025-12-26 21:53:37 +0300

    AutoStart DONE; need check;

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace Clock
 {
@@ -117,6 +118,15 @@ namespace Clock
 			{
 				labelTime.Font = fontDialog.Font;
 			}
+		}
+
+		private void tsmiAutoStart_CheckedChanged(object sender, EventArgs e)
+		{
+			string key_name = "ClockPV_521";
+			RegistryKey rk = Registry.CurrentUser.OpenSubKey("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+			if (tsmiAutoStart.Checked) rk.SetValue(key_name, Application.ExecutablePath);
+			else rk.DeleteValue(key_name, false);   //false - не бросать исключение, если данная запись отсутствует в реестре
+			rk.Dispose();
 		}
 	}
 }
