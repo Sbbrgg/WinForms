ee6858ba89c2fbfc9266d536dbde352908d708d2
un <un@DESKTOP-JAU235M>
2026-01-28 18:22:54 +0300

    Autorunner DONE

---
+++ Autorunner/Autorunner.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Autorunner
+{
+	partial class Autorunner
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBoxLaunch = new System.Windows.Forms.CheckBox();
+			this.buttonComplete = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 58);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(367, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Поздравляем, Вы установили Часы";
+			// 
+			// checkBoxLaunch
+			// 
+			this.checkBoxLaunch.AutoSize = true;
+			this.checkBoxLaunch.Location = new System.Drawing.Point(17, 106);
+			this.checkBoxLaunch.Name = "checkBoxLaunch";
+			this.checkBoxLaunch.Size = new System.Drawing.Size(143, 17);
+			this.checkBoxLaunch.TabIndex = 1;
+			this.checkBoxLaunch.Text = "Запустить приложение";
+			this.checkBoxLaunch.UseVisualStyleBackColor = true;
+			// 
+			// buttonComplete
+			// 
+			this.buttonComplete.Location = new System.Drawing.Point(326, 217);
+			this.buttonComplete.Name = "buttonComplete";
+			this.buttonComplete.Size = new System.Drawing.Size(75, 23);
+			this.buttonComplete.TabIndex = 2;
+			this.buttonComplete.Text = "Завершить";
+			this.buttonComplete.UseVisualStyleBackColor = true;
+			this.buttonComplete.Click += new System.EventHandler(this.buttonComplete_Click);
+			// 
+			// Autorunner
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(418, 252);
+			this.Controls.Add(this.buttonComplete);
+			this.Controls.Add(this.checkBoxLaunch);
+			this.Controls.Add(this.label1);
+			this.Name = "Autorunner";
+			this.Text = "Завершение установки";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBoxLaunch;
+		private System.Windows.Forms.Button buttonComplete;
+	}
+}
+
---
+++ Autorunner/Autorunner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Autorunner
+{
+	public partial class Autorunner : Form
+	{
+		public Autorunner()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonComplete_Click(object sender, EventArgs e)
+		{
+			if(checkBoxLaunch.Checked)
+			{
+				AllocConsole();
+				Directory.SetCurrentDirectory($"{Application.ExecutablePath}\\..");
+				Console.WriteLine(Directory.GetCurrentDirectory());
+				string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+				foreach(string i in files)
+				{
+					Console.WriteLine(i);
+				}
+				Process.Start("Clock.exe");
+			}
+			this.Close();
+		}
+		[DllImport("kernel32.dll")]
+		public extern static bool AllocConsole();
+		[DllImport("kernel32.dll")]
+		public extern static bool FreeConsole();
+	}
+}
